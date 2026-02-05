89f0a2956b39432e84e99cbcb3f8babfb2b73e11
David Cornejo <koerdev@gmail.com>
2026-02-04 22:09:17 -0300

    feat: ejercicio 2 de clases con cuenta bancaria

---
+++ js/05-ejercicio-02.js
@@ -0,0 +1,38 @@
+class CuentaBancaria {
+    constructor(titular, saldoInicial = 0) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    };
+
+    depositar(monto) {
+        if(monto > 0) {
+            this.saldo += monto;
+            return `Deposito exitoso! Su nuevo saldo es: ${this.saldo}`;
+        } else {
+            return "El monto a depositar debe ser mayor a 0";
+        };
+    };
+
+    retirar(monto) {
+        if(monto > 0) {
+            if(this.saldo >= monto) {
+                this.saldo -= monto;
+                return `Retiro exitoso! Su nuevo saldo es: ${this.saldo}`;
+            } else {
+                return "No tiene suficiente saldo para retirar";
+            };
+        } else {
+            return "El monto a depositar debe ser mayor a 0";
+        };
+    };
+
+    mostrarSaldo() {
+        return `Su saldo es de ${this.saldo}`;
+    };
+};
+
+const cuenta1 = new CuentaBancaria("David", 10000);
+
+console.log(cuenta1.mostrarSaldo());
+console.log(cuenta1.depositar(5000));
+console.log(cuenta1.retirar(8000));
